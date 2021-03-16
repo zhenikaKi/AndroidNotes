c383f8d8b93055e07cbdb029f99f54844936ec61
Кирюшин Евгений Андреевич <ZhenikaKi@gmail.com>
2021-03-16 10:02:00 +0500

    Первоначальная структура проекта

---
+++ app/src/main/java/ru/kirea/androidnotes/views/activities/ActivityMain.java
@@ -0,0 +1,15 @@
+package ru.kirea.androidnotes.views.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import ru.kirea.androidnotes.R;
+
+import android.os.Bundle;
+
+public class ActivityMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
