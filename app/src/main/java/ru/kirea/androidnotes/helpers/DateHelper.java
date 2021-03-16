39f9ca698eed7f44de63bfac5d3560cd10dd1b33
Кирюшин Евгений Андреевич <ZhenikaKi@gmail.com>
2021-03-16 11:48:22 +0500

    Первоначальная структура проекта

---
+++ app/src/main/java/ru/kirea/androidnotes/helpers/DateHelper.java
@@ -0,0 +1,24 @@
+package ru.kirea.androidnotes.helpers;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class DateHelper {
+    public enum DateFormat {DDMMYYYY, DDMMYYYY_HHMM, DDMMYYYY_HHMMSS}
+
+    //перевести timestamp в читаемую дату
+    public static String timestampToString(long millis, DateFormat dateFormat) {
+        String patter;
+        if (dateFormat == DateFormat.DDMMYYYY_HHMM) {
+            patter = "dd.MM.yyyy HH:mm";
+        } else if (dateFormat == DateFormat.DDMMYYYY_HHMMSS) {
+            patter = "dd.MM.yyyy HH:mm:ss";
+        } else {
+            patter = "dd.MM.yyyy";
+        }
+
+        SimpleDateFormat simpleFormat = new SimpleDateFormat(patter, Locale.getDefault());
+        return simpleFormat.format(new Date(millis));
+    }
+}
